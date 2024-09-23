eaf9265f31a97e93695bed2db01e4302eb83baaa
jeniffer camily verlindes viana <133382455+camyzj@users.noreply.github.com>
2024-09-23 10:41:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector (".titulo");
+titulo.textContent = "jeniffer nutrição";
+
+var paciente = document.querySelector ("#primeiro paciente");
+
+ var tdPeso = paciente.querySelector(".info-peso");
+ var peso = tdPeso.textContent = "80";
+
+var tdAltura = paciente.querySelector(".info-altura");
+ var altura = tdAltura.textContent = ;
